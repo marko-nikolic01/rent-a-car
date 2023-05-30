0d75f35bcae58035493dd7eb2d99a3d7bf41d479
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-05-30 19:38:41 +0200

    Added packages and User class

---
+++ WebRentACar/src/beans/User.java
@@ -0,0 +1,78 @@
+package beans;
+
+import java.sql.Date;
+
+enum Gender {
+	MALE,
+	FEMALE
+}
+
+enum Role {
+	CUSTOMER,
+	MANAGER,
+	ADMINISTRATOR
+}
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private Date birthday;
+	private Gender gender;
+	private Role role;
+	
+	public User() { }
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+}
