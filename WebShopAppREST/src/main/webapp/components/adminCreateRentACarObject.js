56a0297d41a248606da9ee5a65b19034387f8c0f
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-06-23 19:24:14 +0200

    Added validation for rent a car object creation

--- WebShopAppREST/src/main/webapp/components/adminCreateRentACarObject.js
+++ WebShopAppREST/src/main/webapp/components/adminCreateRentACarObject.js
@@ -15,7 +15,29 @@ Vue.component("adminCreateRentACarObject", {
 			repeatPassword: '',
 			valid: true,
 			unemployedManagersExist: true,
-			now: ''
+			now: '',
+			managers: [],
+			rentACarObject: {
+				id: -1,
+				name: '',
+				workingHours: {
+					startTime: '',
+					endTime: ''
+				},
+				location: {
+					id: -1,
+					longitude: '',
+					latitude: '',
+					address: {
+						city: '',
+						street: '',
+						streetNumber: '',
+						zipCode: ''
+					}
+				},
+				logoURL: '',
+				rating: 3
+			}
 	    }
 	},
 	    template: `
@@ -33,51 +55,51 @@ Vue.component("adminCreateRentACarObject", {
 				<table class="center">
 					<tr>
     					<td><label class="signUpLabel">Name:</label></td>
-        				<td><input type="text" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.name" type="text" class="signUpInput"/></td>
     				</tr>
     				
     				<tr>
     					<td><label class="signUpLabel">Logo (image URL):</label></td>
-        				<td><input type="text" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.logoURL" type="text" class="signUpInput"/></td>
     				</tr>
 				</table>
 			<h4 class="headingCenter">Working time</h4>
 				<table class="center">
 					<tr>
-    					<td><label class="signUpLabel">Start:</label></td>
-        				<td><input type="time" class="signUpInput"/></td>
+    					<td><label  class="signUpLabel">Start:</label></td>
+        				<td><input v-model="rentACarObject.workingHours.startTime" type="time" class="signUpInput"/></td>
     				</tr>
     				
     				<tr>
     					<td><label class="signUpLabel">End:</label></td>
-        				<td><input type="time" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.workingHours.endTime" type="time" class="signUpInput"/></td>
     				</tr>
 				</table>
 			<h4 class="headingCenter">Location info</h4>
 				<table class="center">
 					<tr>
     					<td><label class="signUpLabel">Longitude:</label></td>
-        				<td><input type="number" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.longitude" type="number" class="signUpInput"/></td>
     				</tr>
     				<tr>
     					<td><label class="signUpLabel">Latitude:</label></td>
-        				<td><input type="number" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.latitude" type="number" class="signUpInput"/></td>
     				</tr>
     				<tr>
     					<td><label class="signUpLabel">City:</label></td>
-        				<td><input type="text" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.address.city" type="text" class="signUpInput"/></td>
     				</tr>
     				<tr>
     					<td><label class="signUpLabel">Street:</label></td>
-        				<td><input type="text" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.address.street" type="text" class="signUpInput"/></td>
     				</tr>
     				<tr>
     					<td><label class="signUpLabel">Street number:</label></td>
-        				<td><input type="number" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.address.streetNumber" type="number" class="signUpInput"/></td>
     				</tr>
     				<tr>
     					<td><label class="signUpLabel">ZIP Code:</label></td>
-        				<td><input type="number" class="signUpInput"/></td>
+        				<td><input v-model="rentACarObject.location.address.zipCode" type="number" class="signUpInput"/></td>
     				</tr>
 				</table>
 			<h4 class="headingCenter">Manager</h4>
@@ -126,14 +148,16 @@ Vue.component("adminCreateRentACarObject", {
         				<td><input v-model="user.birthday" type="date" id="birthdayDatePicker" class="signUpInput"/></td>
     				</tr>
     				<br/>
-    				<tr>
+				</table>
+				<table class="center">
+        			<tr>
     					<td></td>
         				<td><input v-on:click="signUp" type="submit" class="button"/></td>
     				</tr>
-				</table>
+        		</table>
         		<table class="center">
     				<tr><td><label v-if="!valid" class="labelError">You didn't fill the form correctly!</label></td></tr>
-        		</table>
+        		</table><br/><br/><br/>
     		</form>
     	</div>
 		`,
@@ -169,20 +193,38 @@ Vue.component("adminCreateRentACarObject", {
 			event.preventDefault();
 			this.validate();
 			if (this.valid) {
-				axios.post("rest/users/", this.user).then(response => (this.userProfile()));
+				return;
 			}
     	},
     	validate : function() {
-			if(this.user.username === ''
+			if(this.rentACarObject.name === ''
+			|| this.rentACarObject.workingHours.startTime === ''
+			|| this.rentACarObject.workingHours.endTime === ''
+			|| (this.rentACarObject.workingHours.startTime > this.rentACarObject.workingHours.endTime)
+			|| this.rentACarObject.location.longitude === ''
+			|| this.rentACarObject.location.latitude === ''
+			|| this.rentACarObject.location.address.city === ''
+			|| this.rentACarObject.location.address.street === ''
+			|| this.rentACarObject.location.address.streetNumber === ''
+			|| this.rentACarObject.location.address.zipCode === ''
+			|| this.rentACarObject.logoURL === '') {
+				this.valid = false;
+				return;
+			}
+			console.log('1');
+			
+			if(!this.unemployedManagersExist && (this.user.username === ''
 			|| this.user.password === ''
 			|| !(this.user.password === this.repeatPassword)
 			|| this.user.firstName === ''
 			|| this.user.lastName === ''
 			|| this.user.birthday === ''
-			|| !(this.user.gender === 'MALE' || this.user.gender === 'FEMALE')) {
+			|| !(this.user.gender === 'MALE' || this.user.gender === 'FEMALE'))) {
 				this.valid = false;
 				return;
 			}
+			console.log('2');
+			
 			this.valid = true;
     	},
 		signOut : function() {
