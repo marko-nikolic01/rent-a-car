616443f62d760c5d5f45535ee0315d4089cc8405
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-06-27 16:36:50 +0200

    Added order class

---
+++ WebShopAppREST/src/main/java/dao/OrderDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.Order;
+
+public class OrderDAO {
+	private String path = "orders.csv";
+	private List<Order> orders = new ArrayList<>();
+	
+	
+}
