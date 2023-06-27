6a1d7e09e13c1e4a7fc3ca44c520ef62fc45e129
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-06-27 16:42:34 +0200

    Added constructor

--- WebShopAppREST/src/main/java/dao/OrderDAO.java
+++ WebShopAppREST/src/main/java/dao/OrderDAO.java
@@ -9,5 +9,14 @@ public class OrderDAO {
 	private String path = "orders.csv";
 	private List<Order> orders = new ArrayList<>();
 	
+	public OrderDAO() {}
 	
+	public OrderDAO(String contextPath) {
+		path = contextPath + this.path;
+		load();
+	}
+	
+	private void load() {
+		
+	}
 }
