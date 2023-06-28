0752f2bf86863aada029285fb9a5242449d5f5e3
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-06-28 15:53:40 +0200

    Added searching in orers

--- WebShopAppREST/src/main/webapp/components/customerUserProfile.js
+++ WebShopAppREST/src/main/webapp/components/customerUserProfile.js
@@ -13,6 +13,16 @@ Vue.component("customerUserProfile", {
 				orders: [],
 				isDeleted: false
 			},
+			orders: [],
+			filteredObjects: [],
+			sortedObjects: [],
+			filter: {
+				objectName: '',
+				minPrice: 0,
+				maxPrice: 99999999,
+				minDate: '',
+				maxDate: ''
+			}
 	    }
 	},
 	    template: `
@@ -26,8 +36,6 @@ Vue.component("customerUserProfile", {
   
   <h4 class="headingCenter">Account info</h4>
   
-  
-			
   <table class="center">
     <tr>
       <td><label class="signUpLabel">Username:</label></td>
@@ -57,8 +65,17 @@ Vue.component("customerUserProfile", {
       <button class="button" v-on:click="editProfile">Edit</button>
     </tr>
   </table>
+  
   <h4 class="headingCenter">Orders</h4>
-  	<div v-for="order in signedInUser.orders" class='container' style="height: 120px;">
+  <label>Rent-A-Car object name:</label><input type="text" v-model="filter.objectName"/>
+  <label>Min. price:</label><input type="number" v-model="filter.minPrice"/>
+  <label>Max. price:</label><input type="number" v-model="filter.maxPrice"/>
+  <label>Min. date:</label><input type="date" v-model="filter.minDate"/>
+  <label>Max. date:</label><input type="date" v-model="filter.maxDate"/>
+  <button v-on:click="filterObjects">Search</button>
+  <button>Cancel search</button>
+  
+  	<div v-for="order in sortedObjects" class='container' style="height: 120px;">
 		<img v-bind:src="order.vehicle.photoURL" height="120" width="150" class="containerImage">
 		<label class="containerLabel">Vehicle: {{order.vehicle.brand}} {{order.vehicle.model}}</label><br/>
 		<label class="containerLabel">Rent a car object: {{order.rentACarObject.name}}</label><br/>
@@ -70,7 +87,9 @@ Vue.component("customerUserProfile", {
 </div>
 	    `,
     mounted () {
-        axios.get("rest/users/signedInUser").then(response => (this.signedInUser = response.data));
+        axios.get("rest/users/signedInUser").then(response => {this.signedInUser = response.data;
+																  this.filteredObjects = structuredClone(this.signedInUser.orders);
+																  this.sortedObjects = structuredClone(this.signedInUser.orders);});
     },
     methods: {
     	editProfile : function() {
@@ -81,6 +100,58 @@ Vue.component("customerUserProfile", {
     	},
     	home : function() {
 			router.push('/customer/home/');
-    	}
+    	},
+    	filterObjects: function() {
+			this.filteredObjects = structuredClone(this.signedInUser.orders);
+			this.sortCriteria = "-";
+			
+			console.log("uspelo");
+			
+			this.filteredObjects = this.filterByObjectName(this.filteredObjects, this.filter.objectName);
+			this.filteredObjects = this.filterByPriceRange(this.filteredObjects, this.filter.minPrice, this.filter.maxPrice);
+			this.filteredObjects = this.filterByDateRange(this.filteredObjects, this.filter.minDate, this.filter.maxDate);
+			
+			this.sortedObjects = structuredClone(this.filteredObjects);
+		},
+		filterByObjectName: function(objects, objectName) {
+			let filtered = [];
+			for (let object of objects) {
+				if (object.rentACarObject.name.toLowerCase().includes(objectName)) {
+					filtered.push(object);
+				}
+			}
+			return filtered;
+		},
+		filterByPriceRange: function(objects, minPrice, maxPrice) {
+			let filtered = [];
+			for (let object of objects) {
+				if (object.price >= minPrice && object.price <= maxPrice) {
+					filtered.push(object);
+				}
+			}
+			return filtered;
+		},
+		filterByDateRange: function(objects, minDate, maxDate) {
+			if (minDate == '') {
+				minDate = '1900-01-01';
+			}
+			if (maxDate == '') {
+				let date = new Date();
+				maxDate = date.toISOString().split('T')[0];
+			}
+			
+			let filtered = [];
+			for (let object of objects) {
+				let dateString = object.orderDateTime.substring(0, 10);
+				let date = new Date(dateString);
+				let lowerDate = new Date(minDate);
+				let upperDate = new Date(maxDate);
+				
+				if (date >= lowerDate && date <= upperDate) {
+					filtered.push(object);
+				}
+			}
+			return filtered;
+		}
     }
 });
