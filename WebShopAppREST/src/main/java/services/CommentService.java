affb0d4edf941a0f26969efed4367451ccdef46c
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-01 13:51:50 +0200

    Added CommentService and a REST API method that gets all comments.

---
+++ WebShopAppREST/src/main/java/services/CommentService.java
@@ -0,0 +1,81 @@
+package services;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import beans.Comment;
+import beans.Order;
+import beans.RentACarObject;
+import beans.User;
+import beans.Vehicle;
+import dao.CommentDAO;
+import dao.OrderDAO;
+import dao.RentACarObjectDAO;
+import dao.UserDAO;
+import dao.VehicleDAO;
+import dto.SignInCredentialsDTO;
+import dto.UserUsernameDTO;
+import utilities.OrderStatus;
+import utilities.RentalStatus;
+
+@Path("/comments")
+public class CommentService {
+	@Context
+	ServletContext servletContext;
+
+	public CommentService() {
+
+	}
+
+	@PostConstruct
+	public void init() {
+		if (servletContext.getAttribute("rentACarObjectDAO") == null) {
+			String contextPath = servletContext.getRealPath("");
+			servletContext.setAttribute("rentACarObjectDAO", new RentACarObjectDAO(contextPath));
+		}
+		if (servletContext.getAttribute("userDAO") == null) {
+			String contextPath = servletContext.getRealPath("");
+			servletContext.setAttribute("userDAO", new UserDAO(contextPath));
+		}
+		if (servletContext.getAttribute("vehicleDAO") == null) {
+			String contextPath = servletContext.getRealPath("");
+			servletContext.setAttribute("vehicleDAO", new VehicleDAO(contextPath));
+		}
+		if (servletContext.getAttribute("orderDAO") == null) {
+			String contextPath = servletContext.getRealPath("");
+			servletContext.setAttribute("orderDAO", new OrderDAO(contextPath));
+		}
+		if (servletContext.getAttribute("commentDAO") == null) {
+			String contextPath = servletContext.getRealPath("");
+			servletContext.setAttribute("commentDAO", new CommentDAO(contextPath));
+		}
+		VehicleDAO vehicleDAO = (VehicleDAO) servletContext.getAttribute("vehicleDAO");
+		RentACarObjectDAO rentACarObjectDAO = (RentACarObjectDAO) servletContext.getAttribute("rentACarObjectDAO");
+		OrderDAO orderDAO = (OrderDAO) servletContext.getAttribute("orderDAO");
+		CommentDAO commentDAO = (CommentDAO) servletContext.getAttribute("commentDAO");
+		Collection<Vehicle> objects = vehicleDAO.getAll();
+		rentACarObjectDAO.linkVehicles(objects);
+		commentDAO.linkOrders(orderDAO.getAll());
+	}
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Comment> getComments() {
+		CommentDAO dao = (CommentDAO) servletContext.getAttribute("commentDAO");
+		return dao.getAll();
+	}
+}
